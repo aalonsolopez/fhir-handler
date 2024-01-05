48c266c680bc51abbd6db82f02055531f38bf6fb
Alejandro Alonso López <aalonsopersonal@gmail.com>
2024-01-05 02:49:52 +0100

    Base Parameter Resource created

--- src/models/FHIRParameterResource.js
+++ src/models/FHIRParameterResource.js
@@ -1,14 +1,32 @@
+/**
+ * FHIR Parameter Resource
+ * @class Parameter
+ */
+
 export default class Parameter extends BaseFHIRResource {
+  /**
+   * Create a Parameter
+   * @param {string} resourceType - The resource type
+   * @param {string} parameter - The list of parameters
+   */
   constructor () {
     super('Parameter')
     this.parameter = []
   }
 
+  /**
+   * Add a parameter to the parameter array
+   * @param {Parameter} parameter 
+   */
   addParameter (parameter) {
     this.parameter.push(parameter)
   }
 
-  getParameter () {
+  /**
+   * Get the parameter array
+   * @returns {Parameter[]}
+  */
+  getParameters () {
     return this.parameter
   }
 }
